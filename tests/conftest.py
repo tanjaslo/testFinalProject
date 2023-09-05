25433768ec1833bba5c30a6f02dd9bcfdebd0d68
Tanja <74559039+tanjaslo@users.noreply.github.com>
2023-09-05 19:38:37 -0400

    Add driver fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
-import pytest
+from test_utils import *
+import pytest as pytest
+from selenium import webdriver
 
 from api.api_client import ApiClient
 
@@ -6,3 +8,11 @@ from api.api_client import ApiClient
 @pytest.fixture
 def api():
     return ApiClient()
+
+
+@pytest.fixture(scope="module")
+def driver():
+    _driver = webdriver.Chrome()
+    _driver.maximize_window()  # Maximize the browser window
+    yield _driver  # Yield the WebDriver instance to the test
+    _driver.quit()  # After the test, close the WebDriver
