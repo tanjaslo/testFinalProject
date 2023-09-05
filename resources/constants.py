89b52b831dd41c38c34ce2e03f3f33877d70d632
Tanja <74559039+tanjaslo@users.noreply.github.com>
2023-09-05 18:26:39 -0400

    Add EXPECTED_FIELDS to constants

--- resources/constants.py
+++ resources/constants.py
@@ -1,1 +1,6 @@
 BASE_URL = "https://8.react.pages.academy/six-cities"
+EXPECTED_FIELDS = {
+    "city", "preview_image", "images", "title", "is_favorite",
+    "is_premium", "rating", "type", "bedrooms", "max_adults",
+    "price", "goods", "host", "description", "location", "id"
+}
