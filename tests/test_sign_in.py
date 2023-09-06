a968fcf8e0d9ea7b128d10a02059084894616214
Tanja <74559039+tanjaslo@users.noreply.github.com>
2023-09-05 21:39:59 -0400

    Minor fixes

--- tests/test_sign_in.py
+++ tests/test_sign_in.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from test_utils import *
 from selenium import webdriver
 import time
@@ -16,19 +13,14 @@ class TestSignIn:
         # Test Case: User Login
         # Initialize the IndexPage class
         index_page = IndexPage(driver)
-
         # Navigate to the website
         index_page.navigate_to(SITE_URL)
-
         # Click on the "Sign in" link
         index_page.wait_and_click_sign_in_link()
-
         # Perform user login
         index_page.signin_user(email_password[0], email_password[1])
-
         # Verify that the user is signed in successfully
         assert index_page.verify_successful_signin(), "User is not signed in successfully"
-
         index_page.sign_out_user()
         time.sleep(1)
         print(f"User {email_password[0]} is signed in successfully and signed out")
@@ -39,12 +31,10 @@ class TestSignIn:
         index_page.navigate_to(SITE_URL)
         index_page.wait_and_click_sign_in_link()
         index_page.signin_user(email_password[0], email_password[1])
-
         time.sleep(1)
         index_page.sign_out_user()
         # Check if the "Sign in" link is visible after signing out
         sign_in_link_visible = index_page.is_sign_in_link_visible()
-
         assert sign_in_link_visible, "Sign in link is not visible after signing out"
         print("User is signed out successfully")
 
@@ -57,7 +47,6 @@ class TestSignIn:
         email_input = driver.find_element(By.NAME, 'email')
         email_input.send_keys("invalid")
         index_page.wait_and_click_sign_in_button()
-
         # Retrieve the validation message
         validation_message = email_input.get_attribute("validationMessage")
         # Check if the validation message matches the expected message
