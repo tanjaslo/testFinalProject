51fa78d83e4d09b0f43f7261b473786a15c320d0
Tanja <74559039+tanjaslo@users.noreply.github.com>
2023-09-07 18:29:14 -0400

    Update sign_in_user method (DRY)

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,6 +1,7 @@
-from selenium.common import NoSuchElementException, TimeoutException
+from selenium.common import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 from pages.index_page_locators import IndexPageLocators
@@ -12,24 +13,32 @@ class IndexPage(BasePage):
         def wait_and_click_sign_in_link(self):
             self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.SIGN_IN_LINK).click()
 
-        def wait_and_type_user_email(self, email):
-            self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.EMAIL_INPUT).send_keys(email)
+        # def wait_and_type_user_email(self, email):
+        #     self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.EMAIL_INPUT).send_keys(email)
+        #
+        # def wait_and_type_user_password(self, password):
+        #     self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.PASSWORD_INPUT).send_keys(password)
 
-        def wait_and_type_user_password(self, password):
-            self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.PASSWORD_INPUT).send_keys(password)
+        def wait_and_type_user_field(self, locator, text):
+            self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, locator).send_keys(text)
 
         def wait_and_click_sign_in_button(self):
             self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.SIGN_IN_BUTTON).click()
 
-        def signin_user(self, email, password):
-            self.wait_and_type_user_email(email)
-            self.wait_and_type_user_password(password)
+        # def signin_user(self, email, password):
+        #     self.wait_and_type_user_email(email)
+        #     self.wait_and_type_user_password(password)
+        #     self.wait_and_click_sign_in_button()
+
+        def sign_in_user(self, email, password):
+            self.wait_and_type_user_field(IndexPageLocators.EMAIL_INPUT, email)
+            self.wait_and_type_user_field(IndexPageLocators.PASSWORD_INPUT, password)
             self.wait_and_click_sign_in_button()
 
         def sign_out_user(self):
             self.explicitly_wait_and_find_element(MAX_WAIT_INTERVAL, IndexPageLocators.SIGN_OUT_LINK).click()
 
-        def verify_successful_signin(self):
+        def verify_successful_sign_in(self):
             try:
                 sign_out_element = self.explicitly_wait_and_find_element(
                     MAX_WAIT_INTERVAL, IndexPageLocators.SIGN_OUT_LINK
