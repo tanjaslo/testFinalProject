36323f8ef532ee6830a898f7559a11d1e34654f8
Tanja <74559039+tanjaslo@users.noreply.github.com>
2023-09-07 19:30:22 -0400

    Delete unnecessary imports

--- pages/index_page.py
+++ pages/index_page.py
@@ -1,7 +1,4 @@
 from selenium.common import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 from pages.index_page_locators import IndexPageLocators
