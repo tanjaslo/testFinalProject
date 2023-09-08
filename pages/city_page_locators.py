d93405d08a53bc56254e0ec18f1fa10a414f6c75
Tanja <74559039+tanjaslo@users.noreply.github.com>
2023-09-07 20:05:07 -0400

    Add CityPageLocators

--- pages/city_page_locators.py
+++ pages/city_page_locators.py
@@ -3,3 +3,4 @@ from selenium.webdriver.common.by import By
 
 class CityPageLocators:
     NUMBER_OF_PLACES = (By.CSS_SELECTOR, "#root>div>main>div.cities>div>section>b")
+    PLACE_CARD = (By.CSS_SELECTOR, '.cities__place-card.place-card')
